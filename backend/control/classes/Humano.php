faedf3ebc26ae7ab75b678273d324979bc7291a7
Filipe Klinger <filipeklinger@gmail.com>
2021-11-28 18:36:01 -0300

    indicando atributos padrao de humano

--- backend/control/classes/Humano.php
+++ backend/control/classes/Humano.php
@@ -4,6 +4,8 @@ namespace Droids\backend\control\classes;
 
 class Humano
 {
+    const OLHOS_COMUNS = "Preto";
+    const IDADE_INICIAL = 0;
     //protected significa que as classes que herdam esta tem acesso a esses atributos
     protected $idade;
     protected $corOlhos;
