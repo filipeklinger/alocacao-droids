568806cf05be7b7e0805d9d4ed6aa402fb8d50af
Filipe Klinger <filipeklinger@gmail.com>
2021-11-28 17:07:15 -0300

    corrigindo separadores de diretorio

--- bibliotecas/autoloadPsr4/Psr4AutoloaderClass.php
+++ bibliotecas/autoloadPsr4/Psr4AutoloaderClass.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace vendor\autoloadPsr4;
 
 /**
@@ -82,7 +83,7 @@ class Psr4AutoloaderClass
         $prefix = trim($prefix, '\\') . '\\';
 
         // normalize the base directory with a trailing separator
-        $base_dir = rtrim($base_dir, DIRECTORY_SEPARATOR) . '/';
+        $base_dir = rtrim($base_dir, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
 
         // initialize the namespace prefix array
         if (isset($this->prefixes[$prefix]) === false) {
@@ -156,8 +157,8 @@ class Psr4AutoloaderClass
             // replace namespace separators with directory separators
             // in the relative class name, append with .php
             $file = $base_dir
-                  . str_replace('\\', '/', $relative_class)
-                  . '.php';
+                . str_replace('\\', DIRECTORY_SEPARATOR, $relative_class)
+                . '.php';
 
             // if the mapped file exists, require it
             if ($this->requireFile($file)) {
